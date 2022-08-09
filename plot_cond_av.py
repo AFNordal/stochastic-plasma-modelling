3e7056db5d347ac69b476848ef36e9c1fedbe639
First Last <rno040@fusion>
2022-08-09 10:00:41 +0200

    new plots

---
+++ plot_cond_av.py
@@ -0,0 +1,29 @@
+import double_exp_ca_analysis as analysis
+import generate_series as datagen
+import storage
+import numpy as np
+import matplotlib.pyplot as plt
+
+gamma = 1
+lmbda = 0.2
+t = 25
+dt = 0.01
+delta = 3
+window = True
+normalize_amplitude = True
+thresh = 2.5
+
+parameters = {
+    "gamma": gamma,
+    "lmbda": lmbda,
+    "t": t,
+    "dt": dt,
+    "delta": delta,
+    "window": window,
+    "normalize_amplitude": normalize_amplitude,
+    "thresh": thresh
+}
+
+times, series = datagen.generate_data(parameters)
+analysis.ca_analysis(times, series, parameters, illustrate=True)
+plt.show()

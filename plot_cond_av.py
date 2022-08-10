351f76283a9de1004bca9e27e5960890a53705ea
AFNordal <nordalrasmus01@gmail.com>
2022-08-10 15:45:37 +0200

    plot_series and plot_cond_av now read from file

--- plot_cond_av.py
+++ plot_cond_av.py
@@ -1,17 +1,15 @@
 import double_exp_ca_analysis as analysis
-import generate_series as datagen
 import storage
-import numpy as np
-import matplotlib.pyplot as plt
 
+display_t = [48, 93]
 gamma = 1
 lmbda = 0.2
-t = 25
+t = 100000
 dt = 0.01
 delta = 3
 window = True
 normalize_amplitude = True
-thresh = 2.5
+thresh = 2
 
 parameters = {
     "gamma": gamma,
@@ -24,6 +22,9 @@ parameters = {
     "thresh": thresh
 }
 
-times, series = datagen.generate_data(parameters)
-analysis.ca_analysis(times, series, parameters, illustrate=True)
-plt.show()
+dir = f"/hdd1/rno040/experiments/n100_gamma{gamma}_lambda0.1-0.5/2/"
+
+times, series = storage.load_series(dir, parameters)
+t0, t1 = tuple(map(lambda x: int(x/dt), display_t))
+analysis.ca_analysis(times[0:t1-t0], series[t0:t1],
+                     parameters, illustrate=True)
--- plot_series.py
+++ plot_series.py
@@ -1,17 +1,17 @@
-import double_exp_ca_analysis as analysis
 import generate_series as datagen
 import storage
 import numpy as np
 import matplotlib.pyplot as plt
 
+display_t = [48, 93]
 gamma = 1
 lmbda = 0.2
-t = 25
+t = 100000
 dt = 0.01
 delta = 3
 window = True
 normalize_amplitude = True
-thresh = 2.5
+thresh = 2
 
 parameters = {
     "gamma": gamma,
@@ -24,6 +24,13 @@ parameters = {
     "thresh": thresh
 }
 
-times, series = datagen.generate_data(parameters)
-plt.plot(times, series)
+dir = f"/hdd1/rno040/experiments/n100_gamma{gamma}_lambda0.1-0.5/2/"
+
+times, series = storage.load_series(dir, parameters)
+
+# times, series = datagen.generate_data(parameters)
+t0, t1 = tuple(map(lambda x: int(x/dt), display_t))
+series = (series-series.mean())/series.std()
+plt.plot(times[0:t1-t0], series[t0:t1], 'k')
+
 plt.show()

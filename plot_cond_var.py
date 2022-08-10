00553dc6c95f4f50af5d39c1aaad7c94920c5523
AFNordal <nordalrasmus01@gmail.com>
2022-08-10 15:47:05 +0200

    modified plots for cond_var

--- plot_cond_var.py
+++ plot_cond_var.py
@@ -2,7 +2,6 @@ import double_exp_ca_analysis as analysis
 from double_exponential_fit import double_exponential
 import storage
 import numpy as np
-import matplotlib
 from matplotlib import pyplot as plt
 
 
@@ -15,30 +14,42 @@ window = True
 normalize_amplitude = True
 thresh = 2.5
 
-parameters = {
-    "gamma": gamma,
-    "lmbda": lmbda,
-    "t": t,
-    "dt": dt,
-    "delta": delta,
-    "window": window,
-    "normalize_amplitude": normalize_amplitude,
-    "thresh": thresh
-}
-
-dir = f"/hdd1/rno040/experiments/n100_gamma{gamma}_lambda0.1-0.5/50/"
-
-ca_data = storage.load_ca_data(dir, parameters)
-svals, s_av, s_var, t_av, peaks, wait = ca_data
-fit_data = analysis.iterate_least_sq(
-    t_av, s_av, parameters)
-fit_time, fit, lmbda_guess, tau_d_guess, err = fit_data
+lines = []
 
 true_time = np.arange(-delta/2, delta/2, dt)
 true_curve = double_exponential(
     len(true_time)//2, true_time, lmbda, lmbda-1, 1, 0)
 
-plt.plot(true_time, true_curve, color="g", linestyle="--", lw=1)
-plt.plot(t_av, s_av, color="b", lw=2)
-plt.plot(t_av, s_var, color="r", linestyle="--", lw=1)
+line, = (plt.plot(true_time, true_curve,
+                  color="red", linestyle="--", lw=2))
+lines.append(line)
+
+
+colors = ['blue', 'magenta', 'green']
+
+for gamma, color in zip((0.1, 1, 10), colors):
+    parameters = {
+        "gamma": gamma,
+        "lmbda": lmbda,
+        "t": t,
+        "dt": dt,
+        "delta": delta,
+        "window": window,
+        "normalize_amplitude": normalize_amplitude,
+        "thresh": thresh
+    }
+
+    dir = f"/hdd1/rno040/experiments/n100_gamma{gamma}_lambda0.1-0.5/50/"
+
+    ca_data = storage.load_ca_data(dir, parameters)
+    svals, s_av, s_var, t_av, peaks, wait = ca_data
+    fit_data = analysis.iterate_least_sq(
+        t_av, s_av, parameters)
+
+    line, = plt.plot(t_av, s_av, color=color, linestyle="-", lw=1)
+    plt.plot(t_av, s_var, color=color, linestyle=":", lw=1)
+
+    lines.append(line)
+
+plt.legend(lines, ("true", 0.1, 1, 10))
 plt.show()
---
+++ plot_variations_cond_var.py
@@ -0,0 +1,101 @@
+from distutils.util import subst_vars
+import double_exp_ca_analysis as analysis
+from double_exponential_fit import double_exponential
+import storage
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+gamma = 1
+lmbda = 0.2
+t = 100000
+dt = 0.01
+delta = 3
+window = True
+normalize_amplitude = True
+thresh = 2.5
+
+lines = []
+
+true_time = np.arange(-delta/2, delta/2, dt)
+true_curve = double_exponential(
+    len(true_time)//2, true_time, lmbda, lmbda-1, 1, 0)
+
+line, = (plt.plot(true_time, true_curve,
+                  color="red", linestyle="--", lw=2))
+lines.append(line)
+
+paramlist = (
+    {
+        "gamma": gamma,
+        "lmbda": lmbda,
+        "t": t,
+        "dt": dt,
+        "delta": delta,
+        "window": window,
+        "normalize_amplitude": normalize_amplitude,
+        "thresh": thresh
+    },
+
+    {
+        "gamma": gamma,
+        "lmbda": lmbda,
+        "t": t,
+        "dt": dt,
+        "delta": delta,
+        "window": not window,
+        "normalize_amplitude": normalize_amplitude,
+        "thresh": thresh
+    },
+
+    {
+        "gamma": gamma,
+        "lmbda": lmbda,
+        "t": t,
+        "dt": dt,
+        "delta": delta,
+        "window": window,
+        "normalize_amplitude": not normalize_amplitude,
+        "thresh": thresh
+    },
+
+    {
+        "gamma": gamma,
+        "lmbda": lmbda,
+        "t": t,
+        "dt": dt,
+        "delta": 6,
+        "window": window,
+        "normalize_amplitude": normalize_amplitude,
+        "thresh": thresh
+    }
+)
+
+dirs = (
+    "/hdd1/rno040/experiments/n100_gamma1_lambda0.1-0.5/0/",
+    "/hdd1/rno040/experiments/win_0/0/",
+    "/hdd1/rno040/experiments/normamp_0/0/",
+    "/hdd1/rno040/experiments/delta_6/0/"
+)
+
+
+colors = ['blue', 'magenta', 'green', 'cyan']
+
+for parameters, dir, color in zip(paramlist, dirs, colors):
+
+    ca_data = storage.load_ca_data(dir, parameters)
+    svals, s_av, s_var, t_av, peaks, wait = ca_data
+    fit_data = analysis.iterate_least_sq(
+        t_av, s_av, parameters)
+
+    line, = plt.plot(t_av[int(len(t_av)/2-delta/(2*dt)):int(len(t_av)/2+delta/(2*dt))],
+                     s_av[int(len(t_av)/2-delta/(2*dt)):int(len(t_av)/2+delta/(2*dt))]/max(s_av),
+                     color=color, linestyle="-", lw=1)
+    plt.plot(t_av[int(len(t_av)/2-delta/(2*dt)):int(len(t_av)/2+delta/(2*dt))],
+             s_var[int(len(t_av)/2-delta/(2*dt)):int(len(t_av)/2+delta/(2*dt))],
+             color=color, linestyle=":", lw=1)
+
+    lines.append(line)
+
+plt.legend(lines, ("true", "base", "win false", "normamp false", "delta 6"))
+plt.show()
